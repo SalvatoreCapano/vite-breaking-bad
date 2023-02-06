61fe82dc5df68fe04d52421b9f8aafe875fd7b93
Salvatore Capano <capano_salvatore@libero.it>
2023-02-06 15:27:58 +0100

    -Inizializzazione Progetto -Header Completato

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Vue 3 + Vite Template
+# Vite Yu-Gi-Oh
+
+Descrizione:
+Create un nuovo progetto utilizzando Vite e Vue 3 e definite i componenti necessari per strutturare il layout come da screenshot allegato.
+Al caricamento della pagina, effettuate una chiama ajax all'API di Yu-Gi-Oh:
+https://ygoprodeck.com/api-guide/
+e con i dati restituiti, stampate una card per ogni personaggio.
+
+Bonus:
+Creare un componente loader da visualizzare fintantoché i risultati non sono pronti.
--- src/App.vue
+++ src/App.vue
@@ -12,15 +12,57 @@
 
 
 <template>
-  <div class="container">
-    <HelloWorld />
-  </div>
+  
+  <header>
+
+    <div class="container">
+
+      <div class="logo">
+        <img src="./assets/logo.webp" alt="Yu-Gi-Oh">
+      </div> <!-- /logo-->
+
+      <h1 class="mainTitle">Yu-Gi-Oh API</h1>
+
+    </div> <!-- /container-->
+
+  </header>
+
+
+  <main>
+
+    <searchForm />
+
+    <resultsCount />
+    
+    <div class="cardsContainer">
+    </div> <!-- /cardsContainer-->
+
+  </main>
+
 </template>
 
-<style>
-  .container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+<style lang="scss">
+@import "style/partials/reset.scss";
+
+  header {
+    background-color: #ffffff;
+    box-shadow: 0px 6px 24px 1px #00000025;
+
+    padding: 16px 0;
+
+    .container {
+      display: flex;
+      align-items: center;
+      gap: 2rem;
+    }
   }
+
+  .logo {
+    height: 80px;
+
+    img {
+      height: 100%;
+    }
+  }
+
 </style>
