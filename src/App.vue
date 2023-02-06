f497842288d659b5fa432ba334b3c3634bcd7582
Salvatore Capano <105233658+SalvatoreCapano@users.noreply.github.com>
2023-02-06 14:36:00 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+
+  import HelloWorld from './components/HelloWorld.vue'
+
+  export default {
+  name: "App",
+  components: {
+      HelloWorld
+  }
+};
+</script>
+
+
+<template>
+  <div class="container">
+    <HelloWorld />
+  </div>
+</template>
+
+<style>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
